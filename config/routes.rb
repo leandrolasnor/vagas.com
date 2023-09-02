89224fab7eb1cecb766fc9febe86cfdd471a64c7
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-02 06:34:03 +0000

    chore: route and locale

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: redirect('rails/info/routes')
+  root to: proc { [200, {}, ['success']] }
 
   namespace :v1 do
     post :vagas, to: 'jobs#create'
     post :pessoas, to: 'peoples#create'
     post :candidaturas, to: 'application#create'
 
-    get '/vagas/:vaga_id/candidaturas/ranking', to: 'jobs#show_applications', as: :ranking
+    get '/vagas/:job_id/candidaturas/ranking', to: 'jobs#ranking', as: :ranking
   end
 
-  get '/health', to: proc { [200, {}, ['success']] }
   match '*path', via: :all, to: proc { [404, {}, nil] }
 end
