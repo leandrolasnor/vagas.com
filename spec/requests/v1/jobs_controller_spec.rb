47a90ef2798ffe282b4eed2aca1aeca76345e1ac
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-03 01:36:34 +0000

    test: test case for requests

--- spec/requests/v1/jobs_controller_spec.rb
+++ spec/requests/v1/jobs_controller_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
-RSpec.describe V1::JobsController do
-  context 'with api version 1' do
+module V1
+  RSpec.describe JobsController do
     describe 'POST /v1/vagas' do
       context 'when params are correct' do
         let(:params) do
@@ -30,6 +29,31 @@ RSpec.describe V1::JobsController do
 
         it 'must be able to create a new job' do
           expect(response).to be_successful
+          expect(json_body).to match(expected_body)
+        end
+      end
+
+      context 'when nivel param is invalid' do
+        let(:params) do
+          {
+            empresa: 'Nome da Empresa',
+            titulo: 'Titulo da Vaga',
+            descricao: 'Descricao da Vaga',
+            localizacao: 'Remoto',
+            nivel: 200
+          }
+        end
+
+        let(:expected_body) do
+          {
+            nivel: ["must be one of: trainee, junior, full, senior, specialist"]
+          }
+        end
+
+        before { post(v1_vagas_path, params: params, as: :json) }
+
+        it 'must be able to get a error message about nivel field' do
+          expect(response).to have_http_status(:unprocessable_entity)
           expect(json_body).to match(expected_body)
         end
       end
