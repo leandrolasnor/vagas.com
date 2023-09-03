b2e6a9a57142adfc8a7d8010a6d5131a4896e4a8
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-03 01:41:21 +0000

    refactor: create application service

--- app/services/http/create_job/contract.rb
+++ app/services/http/create_job/contract.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class Http::CreateJob::Contract < Dry::Validation::Contract
-  params do
-    required(:empresa).filled(:string)
-    required(:titulo).filled(:string)
-    required(:descricao).filled(:string)
-    required(:localizacao).filled(:string)
-    required(:nivel).filled(:integer)
-  end
+  params {}
 end
--- app/services/http/create_job/service.rb
+++ app/services/http/create_job/service.rb
@@ -8,8 +8,12 @@ class Http::CreateJob::Service < Http::Service
 
   def call
     transaction.new.(params) do
-      _1.failure do |e|
-        Rails.logger.error(e)
+      _1.failure :validate do |f|
+        [:unprocessable_entity, f.errors.to_h]
+      end
+
+      _1.failure do |f|
+        Rails.logger.error(f)
         [:internal_server_error]
       end
 
