a1dd4195cd0a5e06ef72f4f70a8b7b949aadc54d
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-04 06:02:47 +0000

    feat: new rule(exists_application) on contract

--- app/use_cases/create_application/contract.rb
+++ app/use_cases/create_application/contract.rb
@@ -7,15 +7,26 @@ class CreateApplication::Contract < ApplicationContract
   end
 
   register_macro(:exists_person) do
-    exists = CreateApplication::Model::Person.exists?(values[:id_pessoa])
-    key(:id_pessoa).failure(:person_not_found) unless exists
+    exists_person = CreateApplication::Model::Person.exists?(values[:id_pessoa])
+    key(:id_pessoa).failure(:person_not_found) unless exists_person
   end
 
   register_macro(:exists_job) do
-    exists = CreateApplication::Model::Job.exists?(values[:id_vaga])
-    key(:id_vaga).failure(:job_not_found) unless exists
+    exists_job = CreateApplication::Model::Job.exists?(values[:id_vaga])
+    key(:id_vaga).failure(:job_not_found) unless exists_job
   end
 
   rule(:id_vaga).validate(:exists_job)
   rule(:id_pessoa).validate(:exists_person)
+
+  register_macro(:exists_application) do
+    exists_application = CreateApplication::Model::Application.exists?(
+      job_id: values[:id_vaga],
+      person_id: values[:id_pessoa]
+    )
+
+    key(:application).failure(:already_applied) if exists_application
+  end
+
+  rule(:id_vaga, :id_pessoa).validate(:exists_application)
 end
