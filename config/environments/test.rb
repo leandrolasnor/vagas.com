95fa3f36a736c46c039ff1d93227ea1bc1940b2b
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-15 14:17:30 +0000

    chore: config logs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
+
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
